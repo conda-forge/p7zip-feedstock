2b3af5d0c487102b72c7d2a1bfa1ab85195631c8
Isuru Fernando <isuruf@gmail.com>
2022-01-09 01:36:58 -0600

    no testing when cross

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,11 @@ if [[ "$target_platform" == osx* ]]; then
     mv "makefile.macosx_llvm_64bits" "makefile.machine"
 fi
 
-make all_test CC=$CC CXX=$CXX ALLFLAGS_C="$CFLAGS" ALLFLAGS_CPP="$CXXFLAGS" LDFLAGS="$LDFLAGS"
+make all3 Client7z LzmaCon CC=$CC CXX=$CXX ALLFLAGS_C="$CFLAGS" ALLFLAGS_CPP="$CXXFLAGS" LDFLAGS="$LDFLAGS"
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    make all_test CC=$CC CXX=$CXX ALLFLAGS_C="$CFLAGS" ALLFLAGS_CPP="$CXXFLAGS" LDFLAGS="$LDFLAGS"
+fi
 
 #sed -i "s|#! /bin/sh|#!/bin/bash|" install.sh
 sed -i.bak "s|DEST_HOME=.*|DEST_HOME=$PREFIX|" install.sh
